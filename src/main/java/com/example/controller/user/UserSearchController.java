66be4a5bd235f3e8e1a6104f9184ae55676dbd58
komamura <vips503@10.253.23.23>
2024-05-20 19:15:50 +0900

    テストコードの調整

--- src/main/java/com/example/controller/user/UserSearchController.java
+++ src/main/java/com/example/controller/user/UserSearchController.java
@@ -49,17 +49,19 @@ public class UserSearchController {
 		// formを個別modelに変換
 		UserModel userModel = modelMapper.map(form, UserModel.class);
 		
-//		UserModel userModel = userSearchService.getUser("000001");
+		// SearchServiceの実行
 		List<UserModel> userList1 = userSearchService.getUser();
 		
 		List<UserModel> userList = new ArrayList<UserModel>();
 		UserModel user = new UserModel();
-		user.setUserId("aaaaa");
-		//user.setUserId(userModel.getUserId());
+		
+		// 画面入力を固定で画面に表示
+		user.setUserId(userModel.getUserId());
+		
+		// 検索した一覧があれば一覧に格納
 		if(!userList1.isEmpty()) {
 			user.setUserName(userList1.get(0).getUserName());
 		}
-		//user.setUserName(userList1.get(0).getUserName());
 		userList.add(user);
 		
 		model.addAttribute("userList",userList);
