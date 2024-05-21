f4c088f3b08386383fd094959b3b33c5424a8bb7
komamura <vips503@10.253.23.23>
2024-05-21 13:20:39 +0900

    コメント追加

--- src/main/java/com/example/util/UtilConst.java
+++ src/main/java/com/example/util/UtilConst.java
@@ -2,13 +2,18 @@ package com.example.util;
 
 public class UtilConst {
 	// マッピングパス
+	/** マッピングパス　/menu */
 	public static final String MAPPING_PATH_MENU = "/menu";
 	
+	/** マッピングパス　/user */
 	public static final String MAPPING_PATH_USER = "/user";
+	/** マッピングパス　/search */
 	public static final String MAPPING_PATH_SEARCH = "/search";
 
 	// レスポンスパス
+	/** レスポンスパス　menu */
 	public static final String RESPONSE_PATH_MENU = "menu";
-	
+
+	/** レスポンスパス　user/userSearch */
 	public static final String RESPONSE_PATH_USER_SEARCH = "user/userSearch";
 }
