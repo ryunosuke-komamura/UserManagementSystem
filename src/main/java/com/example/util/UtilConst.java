2b2dccbf95fb93b5b2ae662efd7f496e00807b1f
komamura <vips503@10.253.23.23>
2024-05-21 14:28:45 +0900

    定数一覧の更新

--- src/main/java/com/example/util/UtilConst.java
+++ src/main/java/com/example/util/UtilConst.java
@@ -7,13 +7,37 @@ public class UtilConst {
 	
 	/** マッピングパス　/user */
 	public static final String MAPPING_PATH_USER = "/user";
-	/** マッピングパス　/search */
+	/** マッピングパス　/qualification */
+	public static final String MAPPING_PATH_QUALIFICATION = "/user";
+	
+	/** マッピングパス　検索　/search */
 	public static final String MAPPING_PATH_SEARCH = "/search";
+	/** マッピングパス　登録　/register */
+	public static final String MAPPING_PATH_REGISTER = "/register";
+	/** マッピングパス　変更　/register */
+	public static final String MAPPING_PATH_UPDATE = "/update";
+	/** マッピングパス　削除　/delete */
+	public static final String MAPPING_PATH_DELETE = "/delete";
+	/** マッピングパス　入力確認　/confirmation */
+	public static final String MAPPING_PATH_CONFIRMATION = "/confirmation";
+	/** マッピングパス　登録実行　/execution */
+	public static final String MAPPING_PATH_EXECUTION = "/execution";
 
 	// レスポンスパス
 	/** レスポンスパス　menu */
 	public static final String RESPONSE_PATH_MENU = "menu";
 
-	/** レスポンスパス　user/userSearch */
+	/** レスポンスパス　ユーザー検索画面　user/userSearch */
 	public static final String RESPONSE_PATH_USER_SEARCH = "user/userSearch";
+	/** レスポンスパス　ユーザー登録変更画面　user/userSearch */
+	public static final String RESPONSE_PATH_USER_REGISTER = "user/userRegister";
+	/** レスポンスパス　ユーザー登録内容確認画面　user/userSearch */
+	public static final String RESPONSE_PATH_USER_CONFIRMATION = "user/userConfirmation";
+
+	/** レスポンスパス　資格検索画面　qualification/qualificationSearch */
+	public static final String RESPONSE_PATH_QUALIFICATION_SEARCH = "qualification/qualificationSearch";
+	/** レスポンスパス　資格登録変更画面　qualification/qualificationSearch */
+	public static final String RESPONSE_PATH_QUALIFICATION_REGISTER = "qualification/qualificationRegister";
+	/** レスポンスパス　資格登録内容確認画面　qualification/qualificationSearch */
+	public static final String RESPONSE_PATH_QUALIFICATION_CONFIRMATION = "qualification/qualificationConfirmation";
 }
