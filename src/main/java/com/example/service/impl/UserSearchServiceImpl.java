b26ac0da0ccca3966235fe504fa550ec01d4a5d3
komamura <vips503@10.253.23.61>
2024-05-22 17:59:29 +0900

    ユーザー検索画面の初期実装

--- src/main/java/com/example/service/impl/UserSearchServiceImpl.java
+++ src/main/java/com/example/service/impl/UserSearchServiceImpl.java
@@ -7,16 +7,27 @@ import org.springframework.stereotype.Service;
 
 import com.example.model.UserModel;
 import com.example.repository.UserMapper;
+import com.example.repository.UserQualMapper;
 import com.example.service.UserSearchService;
 
 @Service
 public class UserSearchServiceImpl implements UserSearchService{
 	
 	@Autowired
-	private UserMapper mapper;
+	private UserMapper userMapper;
+	
+	@Autowired
+	private UserQualMapper userQualMapper;
 	
 	/** ユーザーテーブル検索 */
 	public List<UserModel> getUser(UserModel userModel) {
-		return mapper.findUser(userModel);
+		return userMapper.findUser(userModel);
+	}
+
+	/** ユーザー削除 */
+	public int deleteUser(UserModel userModel) {
+		userQualMapper.deleteUserQual(userModel);
+		
+		return userMapper.deleteUser(userModel);
 	}
 }
