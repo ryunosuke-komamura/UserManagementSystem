6aaa4552a9dceb27417cbd95718d0430426d34e3
komamura <vips503@10.253.23.61>
2024-05-22 18:00:42 +0900

    ユーザー関係のSQL

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -12,4 +12,12 @@ public interface UserMapper {
 	/** ユーザーテーブル検索 */
 	public List<UserModel> findUser(UserModel userModel);
 	
+	/** ユーザーテーブル登録 */
+	public int insertUser(UserModel userModel);
+	
+	/** ユーザーテーブル更新 */
+	public int updateUser(UserModel userModel);
+	
+	/** ユーザーテーブル削除 */
+	public int deleteUser(UserModel userModel);
 }
